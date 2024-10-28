3599e5e4d1b434d26d286d68b89e4553c0609ae8
mayzics-github <mayzics@protonmail.com>
2024-10-28 23:22:13 +0000

    End of section 4

--- API/Program.cs
+++ API/Program.cs
@@ -12,6 +12,7 @@ builder.Services.AddDbContext<StoreContext>(opt =>
   opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 
 var app = builder.Build();
 
@@ -25,7 +26,7 @@ try
   var services = scope.ServiceProvider;
   var context = services.GetRequiredService<StoreContext>();
   await context.Database.MigrateAsync();
-  await StoreContextSeed.SeedAsync(context);  
+  await StoreContextSeed.SeedAsync(context);
 }
 catch (Exception ex)
 {
